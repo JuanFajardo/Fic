55d06d748d2f08c5643f2a0e9d55ad00f6aae9c9
Juan Fajardo <bett0@potosi.bo>
2018-11-23 18:27:10 -0400

    Saltos

--- resources/views/reserva/index.blade.php
+++ resources/views/reserva/index.blade.php
@@ -3,8 +3,10 @@
 @section('cuerpo')
 <div class="row">
   <div class="col-sm">
+    <br><br><br>
     <img src="{{asset('/img/fic.png')}}" alt="" class="circle responsive-img center-align pulse" width="100%"><br>
-    <a href="{{asset('/index.php/reserva')}}" class="btn btn-danger  btn-lg"> <h2>RESERVAR PARA LA F.I.C. 2018</h2></a>
+    <br>
+    <a href="{{asset('/index.php/reserva')}}" class="btn btn-danger  btn-lg"> <h1>RESERVAR PARA LA F.I.C. 2018</h1></a>
   </div>
 </div>
 @endsection
